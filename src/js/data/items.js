7490e14896b587bc29dc62e83b2026475d39205a
Luisoooooooooo <luisalfonsovazquezsanchez@gmail.com>
2026-09-14 16:14:55 +0200

    Feat: Add random item offer

---
+++ src/js/data/items.js
@@ -0,0 +1,187 @@
+export const ITEM_OFFER_SIZE = 10;
+
+export const supportedItems = [
+{
+        id: "leftovers",
+        name: "Restos",
+        effect: "leftovers",
+        description: "Recupera PS al final de cada turno."
+    },
+    {
+        id: "life-orb",
+        name: "Vidasfera",
+        effect: "life-orb",
+        description: "+30% de daño, pero pierde PS al atacar."
+    },
+    {
+        id: "choice-scarf",
+        name: "Pañuelo Elegido",
+        effect: "choice-scarf",
+        description: "Aumenta la Velocidad un 50%."
+    },
+    {
+        id: "choice-band",
+        name: "Cinta Elegida",
+        effect: "choice-band",
+        description: "Aumenta el Ataque un 50%."
+    },
+    {
+        id: "assault-vest",
+        name: "Chaleco Asalto",
+        effect: "assault-vest",
+        description: "Aumenta la Defensa un 50%."
+    },
+    {
+        id: "focus-band",
+        name: "Banda Focus",
+        effect: "focus-band",
+        description: "Puede resistir un KO quedándose a 1 PS."
+    },
+    {
+        id: "quick-claw",
+        name: "Garra Rápida",
+        effect: "quick-claw",
+        description: "Puede hacer que ataques primero."
+    },
+    {
+        id: "sitrus-berry",
+        name: "Baya Zidra",
+        effect: "sitrus-berry",
+        consumable: true,
+        description: "Recupera PS al caer por debajo del 50%."
+    },
+    {
+        id: "rocky-helmet",
+        name: "Casco Dentado",
+        effect: "rocky-helmet",
+        description: "Daña al rival cuando te ataca."
+    },
+    {
+        id: "max-revive",
+        name: "Máx. Revivir",
+        effect: "max-revive",
+        consumable: true,
+        rare: true,
+        description: "Revive con todos sus PS tras caer."
+    },
+
+    {
+        id: "silk-scarf",
+        name: "Pañuelo Seda",
+        effect: "type-boost",
+        boostType: "normal",
+        description: "Aumenta un 20% el daño Normal."
+    },
+    {
+        id: "charcoal",
+        name: "Carbón",
+        effect: "type-boost",
+        boostType: "fire",
+        description: "Aumenta un 20% el daño Fuego."
+    },
+    {
+        id: "mystic-water",
+        name: "Agua Mística",
+        effect: "type-boost",
+        boostType: "water",
+        description: "Aumenta un 20% el daño Agua."
+    },
+    {
+        id: "magnet",
+        name: "Imán",
+        effect: "type-boost",
+        boostType: "electric",
+        description: "Aumenta un 20% el daño Eléctrico."
+    },
+    {
+        id: "miracle-seed",
+        name: "Semilla Milagro",
+        effect: "type-boost",
+        boostType: "grass",
+        description: "Aumenta un 20% el daño Planta."
+    },
+    {
+        id: "never-melt-ice",
+        name: "Antiderretir",
+        effect: "type-boost",
+        boostType: "ice",
+        description: "Aumenta un 20% el daño Hielo."
+    },
+    {
+        id: "black-belt",
+        name: "Cinturón Negro",
+        effect: "type-boost",
+        boostType: "fighting",
+        description: "Aumenta un 20% el daño Lucha."
+    },
+    {
+        id: "poison-barb",
+        name: "Flecha Venenosa",
+        effect: "type-boost",
+        boostType: "poison",
+        description: "Aumenta un 20% el daño Veneno."
+    },
+    {
+        id: "soft-sand",
+        name: "Arena Fina",
+        effect: "type-boost",
+        boostType: "ground",
+        description: "Aumenta un 20% el daño Tierra."
+    },
+    {
+        id: "sharp-beak",
+        name: "Pico Afilado",
+        effect: "type-boost",
+        boostType: "flying",
+        description: "Aumenta un 20% el daño Volador."
+    },
+    {
+        id: "twisted-spoon",
+        name: "Cuchara Torcida",
+        effect: "type-boost",
+        boostType: "psychic",
+        description: "Aumenta un 20% el daño Psíquico."
+    },
+    {
+        id: "silver-powder",
+        name: "Polvo Plata",
+        effect: "type-boost",
+        boostType: "bug",
+        description: "Aumenta un 20% el daño Bicho."
+    },
+    {
+        id: "hard-stone",
+        name: "Piedra Dura",
+        effect: "type-boost",
+        boostType: "rock",
+        description: "Aumenta un 20% el daño Roca."
+    },
+    {
+        id: "spell-tag",
+        name: "Hechizo",
+        effect: "type-boost",
+        boostType: "ghost",
+        description: "Aumenta un 20% el daño Fantasma."
+    },
+    {
+        id: "dragon-fang",
+        name: "Colmillo Dragón",
+        effect: "type-boost",
+        boostType: "dragon",
+        description: "Aumenta un 20% el daño Dragón."
+    },
+    {
+        id: "black-glasses",
+        name: "Gafas de Sol",
+        effect: "type-boost",
+        boostType: "dark",
+        description: "Aumenta un 20% el daño Siniestro."
+    },
+    {
+        id: "metal-coat",
+        name: "Revestimiento Metálico",
+        effect: "type-boost",
+        boostType: "steel",
+        description: "Aumenta un 20% el daño Acero."
+    }
+];
